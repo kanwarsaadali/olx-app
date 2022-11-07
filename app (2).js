16e62010f026d3d3b1705600a77b01d170e2d303
kanwar <Kanwar>
2022-11-07 18:31:03 +0500

    work complete

---
+++ app (2).js
@@ -0,0 +1,23 @@
+var outputScreen = document.getElementById("output-screen");
+
+function display(num){
+    outputScreen.value +=num;
+}
+
+function calculate(){
+    try{
+        outputScreen.value  = eval(outputScreen.value); 
+    }
+    catch(err){
+        alert("Invalid")
+    }
+ 
+}
+
+function remove(){
+    outputScreen.value = "";    
+}
+
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1)
+}
